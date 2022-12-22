a7cedc572af8bbc63447f949aaa2258c8790e124
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2022-12-22 10:56:36 -0300

    !

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -6,8 +6,64 @@ import { FaYoutubeSquare } from "react-icons/fa";
 
 const Footer = () => {
      return (
-          <S.Footer>
-               <S.ItemComposto>
+          <S.Footer className="footer">
+               <S.Item className="item">
+                    <p className="title">Ajuda</p>
+                    <Link to="/teste">
+                         <p>Mapa do Site</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Prazos e locais de entrega</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Política de Troca e Devolução</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Portal de Privacidade</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Nota Fiscal paulista</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Termos e Condições de Uso</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Quantidade de itens por pedido</p>
+                    </Link>
+               </S.Item>
+               <S.Item>
+                    <p className="title">Amerifan</p>
+                    <Link to="/teste">
+                         <p> Serviços</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Blog Amerifan.com.br</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Trabalhe conosco</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Lista de Presentes</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Black Friday</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Venda seus produtos no Amerifan.com.br</p>
+                    </Link>
+                    <Link to="/teste">
+                         <p>Segurança Digital</p>
+                    </Link>
+               </S.Item>
+               <S.Item>
+                    <p className="title">Cartão Amerifan</p>
+               </S.Item>
+          </S.Footer>
+     );
+};
+
+export default Footer;
+/**   <S.ItemComposto>
                     <div>
                          <p className="title">Baixe o nosso App</p>
                          <Link to="teste">
@@ -67,60 +123,4 @@ const Footer = () => {
                               <p>Compre pelo WhatsApp</p>
                          </Link>
                     </div>
-               </S.ItemComposto>
-               <S.Item>
-                    <p className="title">Ajuda</p>
-                    <Link to="/teste">
-                         <p>Mapa do Site</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Prazos e locais de entrega</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Política de Troca e Devolução</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Portal de Privacidade</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Nota Fiscal paulista</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Termos e Condições de Uso</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Quantidade de itens por pedido</p>
-                    </Link>
-               </S.Item>
-               <S.Item>
-                    <p className="title">Amerifan</p>
-                    <Link to="/teste">
-                         <p> Serviços</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Blog Amerifan.com.br</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Trabalhe conosco</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Lista de Presentes</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Black Friday</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Venda seus produtos no Amerifan.com.br</p>
-                    </Link>
-                    <Link to="/teste">
-                         <p>Segurança Digital</p>
-                    </Link>
-               </S.Item>
-               <S.Item>
-                    <p className="title">Cartão Amerifan</p>
-               </S.Item>
-          </S.Footer>
-     );
-};
-
-export default Footer;
+               </S.ItemComposto> */
