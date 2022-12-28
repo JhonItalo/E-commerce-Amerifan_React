335f5847119d2a69b6d7fe6a38a27aa088d33867
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2022-12-28 19:08:38 -0300

    ee

--- src/components/AcessibleButtonName/index.tsx
+++ src/components/AcessibleButtonName/index.tsx
@@ -5,7 +5,7 @@ type props = {
 };
 
 const AccessibleButtonName = ({ name }: props) => {
-     return <p style={{ opacity: "0", position: "absolute", fontSize: "0.2rem" }}>{name}</p>;
+     return <p style={{ opacity: "0", position: "absolute", fontSize: "0.2rem", top: "0" }}>{name}</p>;
 };
 
 export default AccessibleButtonName;
--- src/components/AcessibleName/index.tsx
+++ src/components/AcessibleName/index.tsx
@@ -4,7 +4,7 @@ type props = {
      name: string;
 };
 const AccessibleName = ({ name }: props) => {
-     return <p style={{ position: "absolute", visibility: "hidden", fontSize: "0.2rem" }}>{name}</p>;
+     return <p style={{ position: "absolute", visibility: "hidden", fontSize: "0.2rem", top: "0" }}>{name}</p>;
 };
 
 export default AccessibleName;
