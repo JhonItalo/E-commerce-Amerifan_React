5130cd298a8aaaeb1b013792780b937e3dec4720
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2023-03-25 17:51:23 -0300

    o

--- src/layouts/nav/styles.tsx
+++ src/layouts/nav/styles.tsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 export const Nav = styled.nav`
      width: 100%;
      height: 45px;
-     //padding: 0 6%;
+     display: flex;
+     justify-content: center;
+     align-items: center;
      position: relative;
      background-color: #01022a;
      ul {
