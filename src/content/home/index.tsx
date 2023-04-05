0b7b972c6eaba80d2e55d2bd11d282a83f6e821a
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2023-04-05 19:59:07 -0300

    build: removendo banner da tela incial

--- src/content/home/index.tsx
+++ src/content/home/index.tsx
@@ -4,12 +4,12 @@ import ShowProducts from "../../components/showProducts";
 import RegistrationOffers from "../../components/registrationOffers";
 import SlideCategorys from "../../components/slideCategory";
 import { useContext } from "react";
-import { DataHomeContext } from "../../contexts/DataHomeProvider";
+import { DataHomeContext, DataHomeType } from "../../contexts/DataHomeProvider";
 import BannerSecondary from "../../components/bannerSecondary";
-import * as S from "./styles"
+
 const HomeContent = () => {
-     const { data } = useContext(DataHomeContext);
-     console.log(data, "data");
+     const { data } = useContext<DataHomeType>(DataHomeContext);
+  
      return (
           <>
                <SlideCategorys />
@@ -18,7 +18,6 @@ const HomeContent = () => {
                <ShowProducts title="Best Seller" data={data && data.bestSeller} />
                <BannerSecondary />
                <ShowProducts title="New products" data={data && data.newProducts} />
-               <S.Banner/>
                <RegistrationOffers />
           </>
      );
